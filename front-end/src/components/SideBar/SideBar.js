8ac2ba7a77fd0dc17c6c27ad942af0a81cea5e46
dayannebugarim <81201377+dayannebugarim@users.noreply.github.com>
2022-06-29 16:58:04 -0300

    new

---
+++ front-end/src/components/SideBar/SideBar.js
@@ -0,0 +1,9 @@
+function SideBar() {
+    return(
+        <header className="col-1 col-lg-1 d-flex flex-column justify-content-center sticky-top">
+            
+        </header>
+    )
+}
+
+export default SideBar
